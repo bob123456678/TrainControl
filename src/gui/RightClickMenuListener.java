3f4201d572200589f9265f420042313ee48e7def
Adam <Adam@DESKTOP-CLCRE7D>
2024-11-09 14:23:12 -0700

    Update title

--- src/gui/RightClickMenuListener.java
+++ src/gui/RightClickMenuListener.java
@@ -9,7 +9,6 @@ import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import javax.swing.JButton;
 import javax.swing.JMenuItem;
-import javax.swing.JOptionPane;
 import javax.swing.JPopupMenu;
 import marklin.MarklinLocomotive;
 
