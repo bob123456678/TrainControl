39c72c76ce2998160f0c47c5fc776ecf92ebae9d
Adam <Adam@DESKTOP-CLCRE7D>
2025-01-23 00:42:06 -0700

    Serialization

--- src/org/traincontrol/base/NodeExpression.java
+++ src/org/traincontrol/base/NodeExpression.java
@@ -1,8 +1,10 @@
 package org.traincontrol.base;
 
 import org.traincontrol.model.ViewListener;
+import java.io.Serializable; 
 
-public abstract class NodeExpression
+public abstract class NodeExpression implements Serializable 
 {
+    private static final long serialVersionUID = 1L;
     abstract boolean evaluate(ViewListener network);
 }
